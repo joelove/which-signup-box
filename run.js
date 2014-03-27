83c013cd43959d736dd32e84be36c44c036b0cfb
Joe Love <joe.love@which.co.uk>
2014-03-27 18:57:10 +0000

    Cleaned up and added correct header for JSON content type

--- run.js
+++ run.js
@@ -1,47 +1,43 @@
 /* jshint node: true */
-(function(){
+'use strict';
 
-  'use strict';
+var Datastore = require('nedb'),
+    express = require('express'),
+    db = new Datastore(),
+    app = express();
 
-  var Datastore = require('nedb'),
-      express = require('express'),
-      util = require('util'),
-      db = new Datastore(),
-      app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 
-  app.use(express.json());
-  app.use(express.urlencoded());
-
-  app.get('/latest', function(req, res) {
-    db.find({}).limit(10).exec(function(err, docs){
-      if (err) {
-        res.send('Error selecting records in database.');
-      } else {
-        res.send(util.inspect(docs));
-      }
-    });
-  });
-
-  app.post('/add', function(req, res){
-    var id = req.body.id,
-        doc;
-
-    if(!id) {
-      res.statusCode = 400;
-      res.send('Error 400: Post syntax incorrect.');
-      return;
+app.get('/latest', function(req, res) {
+  db.find({}).limit(10).exec(function(err, docs){
+    if (err) {
+      res.statusCode = 503;
+      res.send('Error 503: Database service unavailable.');
+    } else {
+      res.json(docs);
     }
-
-    doc = {
-      'id' : id,
-      'time' : Date.now()
-    };
-
-    db.insert(doc, function (err) {
-      res.json(err ? false : true);
-    });
   });
+});
 
-  app.listen(process.env.PORT || 4730);
+app.post('/add', function(req, res){
+  var id = req.body.id,
+      doc;
 
-}());
+  if(!id) {
+    res.statusCode = 400;
+    res.send('Error 400: Post syntax incorrect.');
+    return;
+  }
+
+  doc = {
+    'id' : id,
+    'time' : Date.now()
+  };
+
+  db.insert(doc, function (err) {
+    res.json(err ? false : true);
+  });
+});
+
+app.listen(process.env.PORT || 4747);
